50f5d507288af228858716eca8129620abd98379
Sadegh Moayedizadeh <sadegh.moayedizadeh@gmail.com>
2025-07-12 21:52:02 +0330

    Implement the async client model.

---
+++ src/zex/sdk/order.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class OrderSide(str, Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+@dataclass
+class Order:
+    base_token: str
+    quote_token: str
+    side: OrderSide
+    volume: float
+    price: float
