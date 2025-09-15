6ab275d259d77e646d1caafb295cda02d355eb3f
Sadegh Moayedizadeh <sadegh.moayedizadeh@gmail.com>
2025-09-15 17:08:13 +0330

    Fix order data type based on the fields that Zex server returns.

--- src/zex/sdk/data_types/order.py
+++ src/zex/sdk/data_types/order.py
@@ -1,16 +1,20 @@
+from typing import Literal
+
 from pydantic import BaseModel, Field
 
 
 class Order(BaseModel):
-    name: str = Field(..., description="Unique identifier for the order.")
+    amount: float = Field(..., description="Amount of base token to be traded.")
     base_token: str = Field(..., description="Symbol of the base token (e.g., 'ETH').")
-    quote_token: str = Field(
-        ..., description="Symbol of the quote token (e.g., 'USDT')."
+    id: int = Field(..., description="Unique identifier for the order.")
+    name: Literal["buy", "sell"] = Field(
+        ..., description="The name or side of the order."
     )
-    ammount: float = Field(..., description="Amount of base token to be traded.")
-    price: float = Field(..., description="Price per unit of the base token.")
-    t: float = Field(..., description="Timestamp of the order.")  # noqa: VNE001
     nonce: int = Field(
         ..., description="Unique number to ensure order uniqueness and prevent replay."
     )
-    slice: str = Field(..., description="Order slicing information, if applicable.")
+    price: float = Field(..., description="Price per unit of the base token.")
+    quote_token: str = Field(
+        ..., description="Symbol of the quote token (e.g., 'USDT')."
+    )
+    t: float = Field(..., description="Timestamp of the order.")  # noqa: VNE001
